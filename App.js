71e30fa13cd92565c7fbce4bc9803eb247796d1d
k4inos1 <ricardosanhueza09@icloud.com>
2025-06-02 13:54:53 -0400

    fix

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Question Page</h1>
+      </header>
+      <main>
+        <p>This is a question page.</p>
+        <Button variant="contained">Click me</Button>
+      </main>
+      <footer>
+        <p>Copyright 2025</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
